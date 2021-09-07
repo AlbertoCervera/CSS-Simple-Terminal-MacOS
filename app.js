808d897720a16305faa1408c8ca0c41663707dfb
AlbertoCervera <albertocervera20@gmail.com>
2021-09-07 16:04:12 +0200

    First commit

---
+++ app.js
@@ -0,0 +1,9 @@
+var options = {
+  strings: ["¿Hola que tal?", "Bienvenid@ a mi página Web."],
+  typeSpeed: 55,
+  loop: true,
+  loopCount: Infinity,
+  backSpeed: 30,
+};
+
+var typed = new Typed(".element", options);
